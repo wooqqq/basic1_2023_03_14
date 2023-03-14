ebbb6e6aa96432e1a50a3ec0e680cee8cb1c0ace
깃wooqqq <sge0225@naver.com>
2023-03-14 12:58:39 +0900

    RequestParam 추가

--- src/main/java/com/ll/basic1/HomeController.java
+++ src/main/java/com/ll/basic1/HomeController.java
@@ -2,6 +2,7 @@ package com.ll.basic1;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 // @Controller 의 의미
@@ -39,5 +40,16 @@ public class HomeController {
     public int showIncrease() {
         count++;
         return count;
+    }
+
+    @GetMapping("/home/plus")
+    @ResponseBody
+
+    // @RequestParam 의 의미
+    // 개발자가 스프링부트에게 말한다.
+    // int a 는 쿼리스트링에서 a 파라미터의 값을 얻은 후 정수화 한 값이어야 한다.
+    // @RequestParam 는 생략가능
+    public int showPlus(@RequestParam(defaultValue = "0") int a, @RequestParam int b) {
+        return a + b;
     }
 }
