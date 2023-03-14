9f4520f4f99d53fafcda799cd7d492b408023220
깃wooqqq <sge0225@naver.com>
2023-03-14 10:52:39 +0900

    HomeController 추가

---
+++ src/main/java/com/ll/basic1/HomeController.java
@@ -0,0 +1,22 @@
+package com.ll.basic1;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+// @Controller 의 의미
+// 개발자가 스프링부트에게 말한다.
+// 아래 있는 HomeController 는 컨트롤러이다.
+@Controller
+public class HomeController {
+    // @GetMapping("/home/main") 의 의미
+    // 개발자가 스프링부트에게 말한다.
+    // 만약에 /home/main 이런 요청이 오면 아래 메서드를 실행해줘
+    @GetMapping("/home/main")
+    // @ResponseBody 의 의미
+    // 아래 메서드를 실행한 후 그 리턴값을 응답으로 삼아줘
+    @ResponseBody
+    public String showMain() {
+        return "안녕하세요.";
+    }
+}
