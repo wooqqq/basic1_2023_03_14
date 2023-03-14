fece1fecd2426496f39aaf75dd97b8cb7e01ac99
깃wooqqq <sge0225@naver.com>
2023-03-14 14:38:00 +0900

    삭제 설명 추가

--- src/main/java/com/ll/basic1/HomeController.java
+++ src/main/java/com/ll/basic1/HomeController.java
@@ -218,6 +218,10 @@ public class HomeController {
     @GetMapping("/home/removePerson")
     @ResponseBody
     public String removePerson(int id) {
+        // person -> person.getId() == id
+        // 위 함수가 참인 엘리먼트(요소) 경우가 존재하면, 해당 요소를 삭제한다.
+        // removed 에는 삭제수행여부가 저장된다.
+        // 조건에 맞는걸 찾았고 삭제까지 되었다면 true, 아니면 false
         boolean removed = people.removeIf(person -> person.getId() == id);
 
         if (removed == false) {
