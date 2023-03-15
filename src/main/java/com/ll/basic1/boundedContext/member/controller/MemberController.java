98d6582a40621ff66cae42eb19cc201fff0dcdc9
깃wooqqq <sge0225@naver.com>
2023-03-15 15:54:29 +0900

    mapToInt -> mapToLong 버그 수정

--- src/main/java/com/ll/basic1/boundedContext/member/controller/MemberController.java
+++ src/main/java/com/ll/basic1/boundedContext/member/controller/MemberController.java
@@ -68,7 +68,7 @@ public class MemberController {
                     .filter(cookie ->
                             cookie.getName().equals("loginedMemberId"))
                     .map(Cookie::getValue)
-                    .mapToInt(Integer::parseInt)
+                    .mapToLong(Long::parseLong)
                     .findFirst()
                     .orElse(0);
         }
