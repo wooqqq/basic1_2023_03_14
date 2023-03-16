26e663001987ddb08622b74717f546773d3aa363
깃wooqqq <sge0225@naver.com>
2023-03-16 16:02:05 +0900

    me.html에서 로그인 상태에 따른 분기

--- src/main/java/com/ll/basic1/boundedContext/member/entity/Member.java
+++ src/main/java/com/ll/basic1/boundedContext/member/entity/Member.java
@@ -1,10 +1,13 @@
 package com.ll.basic1.boundedContext.member.entity;
 
 import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.Getter;
+import lombok.ToString;
+
 
 @AllArgsConstructor
-@Data
+@Getter
+@ToString
 public class Member {
     private static long lastId;
     private final long id;
