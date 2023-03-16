218c94fff3e43020760464ee795e7a7c932cf4df
깃wooqqq <sge0225@naver.com>
2023-03-16 11:09:11 +0900

    Rq 객체의 생성을 ì스프링에 맡김

--- src/main/java/com/ll/basic1/base/rq/Rq.java
+++ src/main/java/com/ll/basic1/base/rq/Rq.java
@@ -4,9 +4,13 @@ import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.annotation.RequestScope;
 
 import java.util.Arrays;
 
+@Component
+@RequestScope
 @AllArgsConstructor
 public class Rq {
     private final HttpServletRequest req;
--- src/main/java/com/ll/basic1/boundedContext/member/controller/MemberController.java
+++ src/main/java/com/ll/basic1/boundedContext/member/controller/MemberController.java
@@ -4,29 +4,23 @@ import com.ll.basic1.base.rq.Rq;
 import com.ll.basic1.base.rsData.RsData;
 import com.ll.basic1.boundedContext.member.entity.Member;
 import com.ll.basic1.boundedContext.member.service.MemberService;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 
 @Controller
+@AllArgsConstructor
 public class MemberController {
     private final MemberService memberService;
 
     // 생성자 주입
-    @Autowired
-    public MemberController(MemberService memberService) {
-        this.memberService = memberService;
-    }
+    private final Rq rq;
 
     @GetMapping("/member/login")
     @ResponseBody
-    public RsData login(String username, String password, HttpServletRequest req, HttpServletResponse resp) {
-        Rq rq = new Rq(req, resp);
-
+    public RsData login(String username, String password) {
         if (username == null || username.trim().length() == 0) {
             return RsData.of("F-3", "username(을)를 입력해주세요.");
         }
@@ -47,8 +41,7 @@ public class MemberController {
 
     @GetMapping("/member/logout")
     @ResponseBody
-    public RsData logout(HttpServletRequest req, HttpServletResponse resp) {
-        Rq rq = new Rq(req, resp);
+    public RsData logout() {
         boolean cookieRemoved = rq.removeCookie("loginedMemberId");
 
         if (cookieRemoved == false) {
@@ -60,9 +53,7 @@ public class MemberController {
 
     @GetMapping("/member/me")
     @ResponseBody
-    public RsData showMe(HttpServletRequest req, HttpServletResponse resp) {
-        Rq rq = new Rq(req, resp);
-
+    public RsData showMe() {
         long loginedMemberId = rq.getCookieAsLong("loginedMemberId", 0);
 
         boolean isLogined = loginedMemberId > 0;
