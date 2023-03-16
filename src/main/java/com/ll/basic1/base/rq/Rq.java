07e180bf77dde715f55b17604b4df76e00c2655e
깃wooqqq <sge0225@naver.com>
2023-03-16 10:12:19 +0900

    rq 도입

---
+++ src/main/java/com/ll/basic1/base/rq/Rq.java
@@ -0,0 +1,67 @@
+package com.ll.basic1.base.rq;
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.AllArgsConstructor;
+import org.apache.catalina.Context;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+@AllArgsConstructor
+public class Rq {
+    private final HttpServletRequest req;
+    private final HttpServletResponse resp;
+
+    public boolean removeCookie(String name) {
+        if (req.getCookies() != null) {
+            Arrays.stream(req.getCookies())
+                    .filter(cookie -> cookie.getName().equals(name))
+                    .forEach(cookie -> {
+                        cookie.setMaxAge(0);
+                        resp.addCookie(cookie);
+                    });
+
+            return Arrays.stream(req.getCookies())
+                    .filter(cookie -> cookie.getName().equals(name))
+                    .count() > 0;
+        }
+
+        return false;
+    }
+
+    public String getCookie(String name, String defaultValue) {
+        if (req.getCookies() == null) return defaultValue;
+
+        return Arrays.stream(req.getCookies())
+                .filter(cookie -> cookie.getName().equals(name))
+                .map(Cookie::getValue)
+                .findFirst()
+                .orElse(defaultValue);
+    }
+
+    public long getCookieAsLong(String name, long defaultValue) {
+        String value = getCookie(name, null);
+
+        if (value == null) {
+            return defaultValue;
+        }
+
+        try {
+            return Long.parseLong(value);
+        }
+        catch (NumberFormatException e) {
+            return defaultValue;
+        }
+    }
+
+
+    public void setCookie(String name, long value) {
+        setCookie(name, value + "");
+    }
+
+    public void setCookie(String name, String value) {
+        resp.addCookie(new Cookie(name, value));
+    }
+}
--- src/main/java/com/ll/basic1/boundedContext/member/controller/MemberController.java
+++ src/main/java/com/ll/basic1/boundedContext/member/controller/MemberController.java
@@ -1,5 +1,6 @@
 package com.ll.basic1.boundedContext.member.controller;
 
+import com.ll.basic1.base.rq.Rq;
 import com.ll.basic1.base.rsData.RsData;
 import com.ll.basic1.boundedContext.member.entity.Member;
 import com.ll.basic1.boundedContext.member.service.MemberService;
@@ -25,7 +26,8 @@ public class MemberController {
     }
     @GetMapping("/member/login")
     @ResponseBody
-    public RsData login(String username, String password, HttpServletResponse resp) {
+    public RsData login(String username, String password, HttpServletRequest req, HttpServletResponse resp) {
+        Rq rq = new Rq(req, resp);
         if ( username == null || username.trim().length() == 0 ) {
             return RsData.of("F-3", "username(을)를 입력해주세요.");
         }
@@ -38,7 +40,7 @@ public class MemberController {
 
         if (rsData.isSuccess()) {
             Member member = (Member) rsData.getData();
-            resp.addCookie(new Cookie("loginedMemberId", member.getId() + ""));
+            rq.setCookie("loginedMemberId", member.getId());
         }
 
         return rsData;
@@ -47,12 +49,11 @@ public class MemberController {
     @GetMapping("/member/logout")
     @ResponseBody
     public RsData logout(HttpServletRequest req, HttpServletResponse resp) {
-        if (req.getCookies() != null) {
-            Arrays.stream(req.getCookies())
-                    .forEach(cookie -> {
-                        cookie.setMaxAge(0);
-                        resp.addCookie(cookie);
-                    });
+        Rq rq = new Rq(req, resp);
+        boolean cookieRemoved = rq.removeCookie("loginedMemberId");
+
+        if (cookieRemoved == false) {
+            return RsData.of("S-2", "이미 로그아웃 상태입니다.");
         }
 
         return RsData.of("S-1", "로그아웃 되었습니다.");
@@ -60,18 +61,10 @@ public class MemberController {
 
     @GetMapping("/member/me")
     @ResponseBody
-    public RsData showMe(HttpServletRequest req) {
-        long loginedMemberId = 0;
+    public RsData showMe(HttpServletRequest req, HttpServletResponse resp) {
+        Rq rq = new Rq(req, resp);
 
-        if (req.getCookies() != null) {
-            loginedMemberId = Arrays.stream(req.getCookies())
-                    .filter(cookie ->
-                            cookie.getName().equals("loginedMemberId"))
-                    .map(Cookie::getValue)
-                    .mapToLong(Long::parseLong)
-                    .findFirst()
-                    .orElse(0);
-        }
+        long loginedMemberId = rq.getCookieAsLong("loginedMemeberId", 0);
 
         boolean isLogined = loginedMemberId > 0;
 
