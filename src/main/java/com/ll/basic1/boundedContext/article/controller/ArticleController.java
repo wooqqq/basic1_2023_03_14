6231523234e8d652c045069fbf502c95df91dc4d
깃wooqqq <sge0225@naver.com>
2023-03-17 11:45:34 +0900

    article service 도입

--- src/main/java/com/ll/basic1/boundedContext/article/controller/ArticleController.java
+++ src/main/java/com/ll/basic1/boundedContext/article/controller/ArticleController.java
@@ -2,40 +2,32 @@ package com.ll.basic1.boundedContext.article.controller;
 
 import com.ll.basic1.base.rsData.RsData;
 import com.ll.basic1.boundedContext.article.entity.Article;
-import com.ll.basic1.boundedContext.article.repository.ArticleRepository;
+import com.ll.basic1.boundedContext.article.service.ArticleService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.time.LocalDateTime;
-
 @Controller
 @RequestMapping("/article")
 @RequiredArgsConstructor // 필드 중에서 final 붙은것만 인자로 입력받는 생성자 생성
 public class ArticleController {
-    private final ArticleRepository articleRepository;
+    private final ArticleService articleService;
+
     @GetMapping("/write")
     @ResponseBody
     public RsData write(String title, String body) {
-        Article article = Article
-                .builder()
-                .createDate(LocalDateTime.now())
-                .modifyDate(LocalDateTime.now())
-                .title(title)
-                .body(body)
-                .build();
+        if (title == null || title.trim().length() == 0) {
+            return RsData.of("F-1", "title(을)를 입력해주세요.");
+        }
 
-        articleRepository.save(article); // INSERT, or UPDATE
+        if (body == null || body.trim().length() == 0) {
+            return RsData.of("F-2", "body(을)를 입력해주세요.");
+        }
 
-        // 위 코드는 아래 코드와 비슷한 의미이다.
-        /*
-       Article article2 = new Article();
-       article2.setTitlte(title);
-       article2.setBody(body);
-       */
+        Article createdArticle = articleService.write(title, body);
 
-        return RsData.of("S-1", "1번 글이 생성되었습니다.", article);
+        return RsData.of("S-1", "1번 글이 생성되었습니다.", createdArticle);
     }
 }
--- src/main/java/com/ll/basic1/boundedContext/article/repository/ArticleRepository.java
+++ src/main/java/com/ll/basic1/boundedContext/article/repository/ArticleRepository.java
@@ -3,5 +3,6 @@ package com.ll.basic1.boundedContext.article.repository;
 import com.ll.basic1.boundedContext.article.entity.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+// 이 클래스에는 @Repository 어노테이션을 생략해도 된다.
 public interface ArticleRepository extends JpaRepository<Article, Long> {
 }
