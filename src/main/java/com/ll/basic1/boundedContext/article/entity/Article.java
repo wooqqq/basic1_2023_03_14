2bfca4fd0cc04d620e77773317796581247592e5
깃wooqqq <sge0225@naver.com>
2023-03-17 15:29:05 +0900

    등록날짜, 수정날짜 자동갱신

--- src/main/java/com/ll/basic1/boundedContext/article/entity/Article.java
+++ src/main/java/com/ll/basic1/boundedContext/article/entity/Article.java
@@ -1,9 +1,14 @@
 package com.ll.basic1.boundedContext.article.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
@@ -14,11 +19,14 @@ import static jakarta.persistence.GenerationType.IDENTITY;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
 public class Article {
     @Id
     @GeneratedValue(strategy = IDENTITY) // AUTO_INCREMENT
     private long id;
+    @CreatedDate
     private LocalDateTime createDate; // 데이터 생성 날짜
+    @LastModifiedDate
     private LocalDateTime modifyDate; // 데이터 수정 날짜
     private String title;
     private String body;
