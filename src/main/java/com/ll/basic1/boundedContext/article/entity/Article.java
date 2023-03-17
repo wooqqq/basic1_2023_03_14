d26f7087ac6ce8c594eea1138079afbd07f37269
깃wooqqq <sge0225@naver.com>
2023-03-17 11:12:54 +0900

    article entity 수정

--- src/main/java/com/ll/basic1/boundedContext/article/entity/Article.java
+++ src/main/java/com/ll/basic1/boundedContext/article/entity/Article.java
@@ -1,12 +1,20 @@
 package com.ll.basic1.boundedContext.article.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+
+import java.time.LocalDateTime;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
 
 @Entity
 public class Article {
     @Id
+    @GeneratedValue(strategy = IDENTITY) // AUTO_INCREMENT
     private long id;
+    private LocalDateTime createDate; // 데이터 생성 날짜
+    private LocalDateTime modifyDate; // 데이터 수정 날짜
     private String title;
     private String body;
 }
